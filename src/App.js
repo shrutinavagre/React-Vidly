814fdd60e338a99630ef750a5a2a095be613b048
shrutinavagre <shruti.navagre@gmail.com>
2019-06-07 18:24:21 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/common/loginForm";
+import RegisterForm from "./components/common/registerForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import ProtectedRoute from "./components/common/protectedRoute";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar user={this.state.user} />
+        <ToastContainer />
+        <main role="main" className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />; }} />
+            <Route
+              path="/movies"
+              render={props => <Movies {...props} user={this.state.user} />}
+            />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import authService from "../../services/authService";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (!authService.getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: props.location }
+              }}
+            />
+          );
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
